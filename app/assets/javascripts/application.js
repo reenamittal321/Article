d4568098105b93ae8961142b7495243689dabf86
Reena <rmittal054@gmail.com>
2015-09-25 11:30:22 +0530

    Commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+});
+
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+  $('.has-popover').popover({
+    trigger: 'hover'
+  });
+});
