43a48a6ba9d8126a97ba5a32ad253f668a6aa819
Reena <rmittal054@gmail.com>
2015-10-01 10:37:27 +0530

    commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,7 @@ class Ability
         can :read, :all
         can :destroy, Record, :user_id => user.id
         can :destroyed, Record, :user_id => user.id
+        can :destroy_multiple, Record, :user_id => user.id
     #   end
     #
     # The first argument to `can` is the action you are giving the user
